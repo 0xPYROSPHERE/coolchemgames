170998c5df92f6af5de0bd736eb393244e7d0158
PYRØSPHERE <domjr1126@gmail.com>
2026-03-08 13:47:37 -0400

    test3

--- emulatorjs/data/loader.js
+++ emulatorjs/data/loader.js
@@ -58,4 +58,96 @@
                             }
                         }
                     }
-})
+                    
+                    if (('undefined' != typeof EJS_DEBUG_XX && true === EJS_DEBUG_XX)) {
+                        await loadScript('emulator.js');
+                        await loadScript('nipplejs.js');
+                        await loadScript('shaders.js');
+                        await loadScript('storage.js');
+                        await loadScript('gamepad.js');
+                        await loadScript('GameManager.js');
+                        await loadScript('socket.io.min.js');
+                        await loadStyle('emulator.css');
+                    } else {
+                        await loadScript('emulator.min.js');
+                        await loadStyle('emulator.min.css');
+                    }
+                    const config = {};
+                    config.gameUrl = window.EJS_gameUrl;
+                    config.dataPath = scriptPath;
+                    config.system = window.EJS_core;
+                    config.biosUrl = window.EJS_biosUrl;
+                    config.gameName = window.EJS_gameName;
+                    config.color = window.EJS_color;
+                    config.adUrl = window.EJS_AdUrl;
+                    config.adMode = window.EJS_AdMode;
+                    config.adTimer = window.EJS_AdTimer;
+                    config.adSize = window.EJS_AdSize;
+                    config.alignStartButton = window.EJS_alignStartButton;
+                    config.VirtualGamepadSettings = window.EJS_VirtualGamepadSettings;
+                    config.buttonOpts = window.EJS_Buttons;
+                    config.volume = window.EJS_volume;
+                    config.defaultControllers = window.EJS_defaultControls;
+                    config.startOnLoad = window.EJS_startOnLoaded;
+                    config.fullscreenOnLoad = window.EJS_fullscreenOnLoaded;
+                    config.filePaths = window.EJS_paths;
+                    config.loadState = window.EJS_loadStateURL;
+                    config.cacheLimit = window.EJS_CacheLimit;
+                    config.cheats = window.EJS_cheats;
+                    config.defaultOptions = window.EJS_defaultOptions;
+                    config.gamePatchUrl = window.EJS_gamePatchUrl;
+                    config.gameParentUrl = window.EJS_gameParentUrl;
+                    config.netplayUrl = window.EJS_netplayServer;
+                    config.gameId = window.EJS_gameID;
+                    config.backgroundImg = window.EJS_backgroundImage;
+                    config.backgroundBlur = window.EJS_backgroundBlur;
+                    config.backgroundColor = window.EJS_backgroundColor;
+                    config.controlScheme = window.EJS_controlScheme;
+                    config.threads = window.EJS_threads;
+                    config.disableCue = window.EJS_disableCue;
+                    config.startBtnName = window.EJS_startButtonName;
+                    config.softLoad = window.EJS_softLoad;
+                    config.screenRecording = window.EJS_screenRecording;
+                    config.externalFiles = window.EJS_externalFiles;
+                    config.disableDatabases = window.EJS_disableDatabases;
+                    config.disableLocalStorage = window.EJS_disableLocalStorage;
+                    config.forceLegacyCores = window.EJS_forceLegacyCores;
+                    config.noAutoFocus = window.EJS_noAutoFocus;
+                    config.shaders = Object.assign({}, window.EJS_SHADERS, window.EJS_shaders ? window.EJS_shaders : {});
+                    
+                    if (typeof window.EJS_language === "string" && window.EJS_language !== "en-US") {
+                        try {
+                            let path;
+                            if ('undefined' != typeof EJS_paths && typeof EJS_paths[window.EJS_language] === 'string') {
+                                path = EJS_paths[window.EJS_language];
+                            } else {
+                                path = scriptPath+"localization/"+window.EJS_language+".json";
+                            }
+                            config.language = window.EJS_language;
+                            config.langJson = JSON.parse(await (await fetch(path)).text());
+                        } catch(e) {
+                            config.langJson = {};
+                        }
+                    }
+                    
+                    window.EJS_emulator = new EmulatorJS(EJS_player, config);
+                    window.EJS_adBlocked = (url, del) => window.EJS_emulator.adBlocked(url, del);
+                    if (typeof window.EJS_ready === "function") {
+                        window.EJS_emulator.on("ready", window.EJS_ready);
+                    }
+                    if (typeof window.EJS_onGameStart === "function") {
+                        window.EJS_emulator.on("start", window.EJS_onGameStart);
+                    }
+                    if (typeof window.EJS_onLoadState === "function") {
+                        window.EJS_emulator.on("loadState", window.EJS_onLoadState);
+                    }
+                    if (typeof window.EJS_onSaveState === "function") {
+                        window.EJS_emulator.on("saveState", window.EJS_onSaveState);
+                    }
+                    if (typeof window.EJS_onLoadSave === "function") {
+                        window.EJS_emulator.on("loadSave", window.EJS_onLoadSave);
+                    }
+                    if (typeof window.EJS_onSaveSave === "function") {
+                        window.EJS_emulator.on("saveSave", window.EJS_onSaveSave);
+                    }
+                })();
