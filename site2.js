d7d177c708d750d090c0f4f6ac4ed27e4b1e696d
PYRØSPHERE <domjr1126@gmail.com>
2026-05-08 06:46:21 -0400

    testing

--- site2.js
+++ site2.js
@@ -1,8 +1,39 @@
-var siteFrame = document.getElementById("site");
-fetch("index.html").then(response => response.text()).then(response => {siteFrame.srcdoc = response});
+const siteFrame = document.getElementById("site");
+fetch(`embed.html?v=${Math.random() * 100000}`).then(response => response.text()).then(response => {siteFrame.srcdoc = response});
 siteFrame.addEventListener("fullscreenchange", () => {
     siteFrame.style.display = document.fullscreenElement ? "block" : "none"
 });
 document.getElementById("open").addEventListener("click", () => {
     siteFrame.requestFullscreen();
 });
+document.getElementById("new").addEventListener("click", () => {
+    const siteWindow = window.open("", "_blank");
+    siteWindow.document.write(`
+        <html>
+            <head>
+                <title>Cool Chem Games</title>
+                <style>
+                    body, html {
+                        margin: 0;
+                        padding: 0;
+                        width: 100%;
+                        height: 100%;
+                        overflow: hidden;
+                    }
+                </style>
+            </head>
+            <body>
+                <iframe id="main" srcdoc=""></iframe>
+                <script>
+                    var loaded = false;
+                    window.addEventListener("message", (e) => {
+                            if (!loaded) {
+                                document.getElementById("main").srcdoc = e.data;
+                            }
+                        });
+                <\/script>
+            </body>
+        </html>
+    `);
+    siteWindow.postMessage(siteFrame.srcdoc, "*");
+})
