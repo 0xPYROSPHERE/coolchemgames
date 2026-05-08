f40aa8359b9855c17089ea8d3c8625ffbd1cace3
PYRØSPHERE <domjr1126@gmail.com>
2026-05-08 15:27:18 -0400

    test

--- embed6.js
+++ embed6.js
@@ -1,6 +1,18 @@
+var currentVersion = 30;
+
 async function fetchSite() {
-    let htmlString = await fetch(`embed.html?v=${Math.random() * 100000}`)
-    htmlString = await htmlString.text();
+    let htmlString = "";
+    let foundVersion = false;
+    let i = currentVersion;
+    while (!foundVersion) {
+        let fetchRequest = await fetch(`embed${i}.html`);
+        if (fetchRequest.status != 403) {
+            foundVersion = true;
+            currentVersion = i;
+            htmlString = await fetchRequest.text();
+        }
+        i--;
+    }
     return htmlString;
 }
 
