87f727fdc7274d818deeeadd26be90d042ba13b2
PYRØSPHERE <domjr1126@gmail.com>
2026-05-07 20:41:51 -0400

    2

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 var siteFrame = document.getElementById("site");
 fetch("index.html").then(response => response.text()).then(response => {siteFrame.srcdoc = response});
 siteFrame.addEventListener("fullscreenchange", () => {
-    siteFrame.display = document.siteFrame ? "block" : "none"
+    siteFrame.display = document.fullscreenElement ? "block" : "none"
 });
 document.getElementById("open").addEventListener("click", () => {
     siteFrame.requestFullscreen();
