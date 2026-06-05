6c59be8702a5a146ce079854f5f8d8131fb63600
PYRØSPHERE <domjr1126@gmail.com>
2026-06-04 20:45:23 -0400

    test stuff

---
+++ agsvg.js
@@ -0,0 +1,12 @@
+async function fetchSite() {
+    let date = new Date();
+    let start = new Date();
+    start.setDate(0);
+    let htmlString = await fetch(`https://cdn.jsdelivr.net/gh/0xPYROSPHERE/coolchemgames/algeflix.${date.getMonth() + 1}-${Math.ceil((date.getDate() + ((start.getDay() + 1) % 7 - 1)) / 7)}.html?v=${Math.floor(Math.random() * 100000)}`)
+    htmlString = await htmlString.text();
+    return htmlString;
+}
+
+const siteFrame = document.getElementById("site");
+fetchSite().then(data => {siteFrame.srcdoc = data;});
+const siteHtml = siteFrame.srcdoc;
