0a1e2e5f669a016e2f3bb4f690a3d67217cbcc50
PYRØSPHERE <domjr1126@gmail.com>
2026-06-11 20:07:35 -0400

    test

--- agsvg2.js
+++ agsvg2.js
@@ -2,8 +2,8 @@ async function fetchSite() {
     let date = new Date();
     let start = new Date();
     start.setDate(0);
-    let name = window.location.search ? "player" : "algeflix";
-    let htmlString = await fetch(`https://cdn.jsdelivr.net/gh/0xPYROSPHERE/coolchemgames/${name}.${date.getMonth() + 1}-${Math.ceil((date.getDate() + ((start.getDay() + 1) % 7 - 1)) / 7)}.html`)
+    let name = window.location.search ? "player" : `algeflix.${date.getMonth() + 1}-${Math.ceil((date.getDate() + ((start.getDay() + 1) % 7 - 1)) / 7)}`;
+    let htmlString = await fetch(`https://cdn.jsdelivr.net/gh/0xPYROSPHERE/coolchemgames/${name}.html`)
     htmlString = await htmlString.text();
     return htmlString;
 }
